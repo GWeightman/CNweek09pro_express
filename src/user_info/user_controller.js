56446b5f68accbb0a0df290a164fc9272d89d1a9
GWeightman <g.weightman@hotmail.co.uk>
2022-01-05 18:13:11 +0000

    CRUD complete

--- src/user_info/user_controller.js
+++ src/user_info/user_controller.js
@@ -19,3 +19,28 @@ exports.delete_user = async (req, res) => {
         res.status(500).send({ message: "check server logs" })
     }
 }
+
+exports.list_user = async (req, res) => {
+    try {
+      const user_entry = await User.findOne({ username: req.params.username });
+      res.status(200).send(user_entry);
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({ message: "Check server logs" });
+    }
+  };
+  
+  exports.update_user = async (req, res) => {
+    try {
+      const user_entry = await User.updateOne(
+        { username: req.body.username },
+        { $set: { password: req.body.password } },
+        { new: true }
+      );
+      res.status(200).send({ message: "Success", user_entry });
+    } catch (error) {
+      console.log(error);
+      res.status(500).send({ message: "Check server logs" });
+    }
+  };
+  
--- src/user_info/user_routes.js
+++ src/user_info/user_routes.js
@@ -1,11 +1,11 @@
 const { Router } = require("express");
 const { hash_pass } = require("../middleware/hash");
 const user_router = Router()
-const { add_user, delete_user } = require("./user_controller")
+const { add_user, delete_user, update_user, list_user } = require("./user_controller")
 
 user_router.post("/user", hash_pass, add_user);
 user_router.delete("/user/:username", delete_user)
-// user_router.put("/user", edit_user)
-// user_router.get("/user", list_user)
+user_router.put("/user", hash_pass, update_user)
+user_router.get("/user/:username", list_user)
 
 module.exports = user_router
