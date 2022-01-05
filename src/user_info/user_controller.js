e220b0cbbf8d2ce021412bf8140788902242ea25
GWeightman <g.weightman@hotmail.co.uk>
2022-01-05 17:58:50 +0000

    delete added

--- src/user_info/user_controller.js
+++ src/user_info/user_controller.js
@@ -9,3 +9,13 @@ exports.add_user = async (req, res) => {
         res.status(500).send({ message: "check server logs" })
     }
 };
+
+exports.delete_user = async (req, res) => {
+    try {
+        const user_entry = await User.deleteOne({username: req.params.username})
+        res.status(200).send({ message: "Successfully deleted", user_entry })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({ message: "check server logs" })
+    }
+}
--- src/user_info/user_routes.js
+++ src/user_info/user_routes.js
@@ -1,10 +1,10 @@
 const { Router } = require("express");
 const { hash_pass } = require("../middleware/hash");
 const user_router = Router()
-const { add_user } = require("./user_controller")
+const { add_user, delete_user } = require("./user_controller")
 
 user_router.post("/user", hash_pass, add_user);
-// user_router.delete("/user", delete_user)
+user_router.delete("/user/:username", delete_user)
 // user_router.put("/user", edit_user)
 // user_router.get("/user", list_user)
 
