35d0206ac3c8864c57eb390d4ffcbdecfc36ce8e
GWeightman <g.weightman@hotmail.co.uk>
2022-01-11 10:25:22 +0000

    cors added

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 require("./db/connection")
 const express = require("express")
+const cors = require("cors")
 const app = express()
 const port = process.env.PORT || 5000
 const user_router = require("./user_info/user_routes")
 
 app.use(express.json())
+app.use(cors())
 app.use(user_router)
 
 app.listen(port, () => {
