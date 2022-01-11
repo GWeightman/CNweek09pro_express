8842351c0a22ddfddae436fb0b0a179668fbefe1
GWeightman <g.weightman@hotmail.co.uk>
2022-01-11 19:06:37 +0000

    minor updates

--- src/user_info/user_routes.js
+++ src/user_info/user_routes.js
@@ -7,7 +7,7 @@ const { add_user, delete_user, update_user, list_user, token_check } = require("
 user_router.post("/user", hash_pass, add_user);
 user_router.delete("/user/:username", delete_user)
 user_router.put("/user", hash_pass, update_user)
-user_router.get("/user/:username", hash_compare, list_user)
+user_router.get("/user/:username", list_user)
 user_router.get("/user", token_check)
 
 module.exports = user_router
